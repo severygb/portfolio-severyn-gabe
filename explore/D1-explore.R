8cc90cfa4b25f3255fcd2a613d819dc2ef9598a6
severygb <severygb@rose-hulman.edu>
2020-04-09 14:19:32 -0400

    A few changes to D1 and notes to myself in the comments file

--- explore/D1-explore.R
+++ explore/D1-explore.R
@@ -20,27 +20,35 @@ summary(college_raw)
 #category 1: School Type. Levels: public, private
 #category 2: ACT Performance. Levels: above average, below average
 
-college <- select(college_raw, c("Public", "Earn", "ACT")) %>% glimpse()
+college <- select(college_raw, c("Public", "Earn", "ACT","need_fraction")) %>% glimpse()
+
+#eliminate rows with NA's
+college <- na.omit(college)
 
 #create above/below average ACT data
 college <- college %>%
-  mutate("ACT_Average" = ifelse(college$ACT > mean(college$ACT), "Above Average", "Below Average")) %>%
+  mutate("ACT_rank" = ifelse(college$ACT > mean(college$ACT), "High ACT", "Low ACT")) %>%
+  glimpse()
+
+#create high/low need data
+college <- college %>%
+  mutate("need_rank" = ifelse(college$need_fraction > mean(college$need_fraction), "High Need", "Low Need")) %>%
   glimpse()
 
 #turn Public into a category
 college <- college %>%
-  mutate("Type" = ifelse(college$Public, "Public School", "Private School")) %>%
+  mutate("school_type" = ifelse(college$Public, "Public School", "Private School")) %>%
   glimpse()
 
 #turn 4 combinations into 4 levels & reorder
 college <- college %>% 
-  mutate(type_performance = str_c(ACT_Average, Type, sep = " @ ")) %>% 
-  mutate(type_performance = fct_reorder(type_performance, Earn))
+  mutate(category = str_c(ACT_rank, need_rank, sep = " & ")) %>% 
+  mutate(category = fct_reorder(category, Earn)) #default ranking is median
 
-p <- ggplot(college, aes(x = Earn, y = type_performance, color = Type, fill = Type)) +
+p <- ggplot(college, aes(x = Earn, y = category, color = school_type, fill = school_type)) +
   geom_jitter(width = 0, height = 0.2, shape = 21, size = 2, alpha = 0.5) +
   theme_graphclass() +
-  labs(x = "Post-Graduation Earnings", y = "")
+  labs(x = "Annual Earnings Post-Graduation", y = "")
 p
 
 summary(college)
