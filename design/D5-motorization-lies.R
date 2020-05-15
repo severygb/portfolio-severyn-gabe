9513c8e3e6c53cae3045fe1d748c4c95dd9fb32c
severygb <severygb@rose-hulman.edu>
2020-05-14 22:21:18 -0400

    more d5 work

--- design/D5-motorization-lies.R
+++ design/D5-motorization-lies.R
@@ -11,7 +11,7 @@ world <- map_data("world")
 map <- ggplot(world, aes(x = long, y = lat, group = group)) +
   geom_polygon(fill = "gray") +
   coord_map(projection = "mollweide", 
-            ylim = c(-75, 75), 
+            ylim = c(-50, 75), 
             orientation = c(90, 0, 0)) +
   theme(axis.line=element_blank(),
         axis.text.x=element_blank(),
@@ -25,24 +25,53 @@ map <- ggplot(world, aes(x = long, y = lat, group = group)) +
         panel.grid.minor=element_blank(),
         plot.background=element_blank()
   )
-map
 
 #adds layer for color gradient
-map %<>% +
+map <- map +
   geom_polygon(data = map_data, aes(x = long, y = lat, group = group, fill = motor_rate2015)) +
-  labs(title = "Motorization rate in 2015 (per 1000 people)", caption = "Source: OICA") +
+  labs(title = "Motorization rate in 2015", caption = "Source: OICA", fill = "Vehicles\nper 1000 people") +
   scale_colour_distiller(
-  type = "seq",
-  palette = "Oranges",
-  direction = 1,
-  space = "Lab",
-  guide = "colourbar",
-  aesthetics = "fill"
-)
+    type = "seq",
+    palette = "Oranges",
+    direction = 1,
+    space = "Lab",
+    guide = "colourbar",
+    aesthetics = "fill"
+    ) +
+  theme(legend.position = c(0.2,0.25),
+        plot.title = element_text(size = 11),
+        plot.caption = element_text(size = 11),
+        legend.title = element_text(size = 10),
+        legend.background = element_rect(fill = NA))
+
+map
+
+ggsave(plot = map, filename = "D5-map.png",
+       path    = "figures",
+       width   = 10,
+       height  = 5,
+       units   = "in",
+       dpi     = 300)
+
 
 #dot plot
 dot_plot_data <- readRDS("data/D5-motorization-dot-plot-data.rds")
 
-ggplot(continents_data, aes(x = motor_rate2015, y = region)) +
-  geom_point() +
-  theme_graphclass()
+dot_plot <- ggplot(continents_data, aes(x = motor_rate2015, y = region)) +
+  geom_point(size = 2) +
+  theme_graphclass() +
+  theme(
+    plot.margin = unit(c(0.1, 0.25, 0.1, 0), unit = "in")
+  ) +
+  labs(x = "Vehicles per 1000 people", y = "", caption = "Source: OICA",
+       title = "Motorization rate in 2015") +
+  scale_x_continuous(breaks = c(0,200,400,600,800))
+
+dot_plot
+
+ggsave(plot = dot_plot, filename = "D5-dot-plot.png",
+       path    = "figures",
+       width   = 6,
+       height  = 4,
+       units   = "in",
+       dpi     = 300)
---
+++ reports/D5-report.md
@@ -0,0 +1,60 @@
+revise graphical lie
+================
+
+What kind of lie is it? (See graphical lies slides for types) Look to
+put the data in context: add more years? surrounding areas?
+
+The map projection of original is true, uses the Molleweide projection
+which more accurately portrays land area (need a citation). Many graphs
+use the Mercatur projection, which distorts the globe in such a way that
+North American and European countries seem larger.
+
+They avoided value laden colors - their scheme has absolutely no
+meaning. Color could be used to make certain countries or continents
+more prominent or impart emotional or political biases.
+
+The countries are grouped, roughly by continent. I can only deduce this
+was necessary because the map does not have enough space to write each
+countrie’s data. Grouping by global region means a single mean can be
+displayed, which is representative of that region’s data. Since I am
+representing each countries motorization rate on a color scale and not
+in a written number, the groupings are not necessary for the data to be
+legible.
+
+<img src="../figures/D5-map.png" width="100%" />
+
+<img src="../figures/D5-dot-plot.png" width="60%" />
+
+## Graph design
+
+Data requirements
+
+  - Minimum 20 observations
+  - One quantitative variable conveying information about human injury
+    or fatality
+  - One or two categorical variables, one of which can be time (time
+    series)
+
+Normalizing the fatalities would be more appropriate in this case
+because it would account for the changing number of participants.
+However, after searching, I could not find data for the number of
+participants across all racing types and series to match the fatalities
+context.
+
+Why is this graph type appropriate? design decisions, especially lie
+content and how I fixed it
+
+“showing data more than one way increases our understanding” (Robbins,
+[2005](#ref-Robbins:2005)).
+
+## References
+
+<div id="refs" class="references">
+
+<div id="ref-Robbins:2005">
+
+Robbins N (2005) Scales. *Creating More Effective Graphs*. 226–291
+
+</div>
+
+</div>
